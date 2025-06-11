fc8abfebeae3d9d83c3ded478e91a1fd0fdd456e
Dirceu Wilian de Camargo Conte <wilianconte@gmail.com>
2025-06-11 00:21:50 -0300

    Refactor index page into reusable components

---
+++ components/PageHead.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function PageHead() {
+  return (
+    <Head>
+      <title>Mundo da Lua</title>
+      <link rel="stylesheet" href="https://meyerweb.com/eric/tools/css/reset/reset.css" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+    </Head>
+  );
+}
---
+++ components/sections/FirstSection.js
@@ -0,0 +1,17 @@
+export default function FirstSection() {
+  return (
+    <div className="first">
+      <div className="first-wrapper">
+        <div className="left">
+          <p className="fs-2xl desktop-fs-8xl fw-600">Novos horizontes aguardam seu filho</p>
+          <p className="fs-md  desktop-fs-2xl fw-400">Nossos tutores ajudam seu pequeno a desbloquear todo o potencial guardado dentro dele.</p>
+          <a className="btn btn-secondary" href="#">Marcar aula experimental</a>
+        </div>
+        <div className="right">
+          <img src="/img/slide-1.jpg" alt="slide" />
+          <img src="/img/slide-1-lg.jpg" alt="slide" />
+        </div>
+      </div>
+    </div>
+  );
+}
